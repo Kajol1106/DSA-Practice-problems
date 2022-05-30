42e7b93f106343cd46a9658973cb130918aaf7a4
Kajol1106 <kolagirkajol@gmail.com>
2022-05-31 00:26:21 +0530

    star pattern 1

---
+++ Pattern Printing/Star Pattern/pattern1.js
@@ -0,0 +1,25 @@
+/* 
+ output=  * 
+          * * 
+          * * *
+          * * * *
+    
+*/
+
+//Using For loop
+// let n=4;
+// for(let i=1; i<=n; i++){
+//     console.log("* ".repeat(i));
+//  }
+
+
+//Using While Loop
+let n=4;
+let i=0;
+while(i<=n)
+{
+    console.log("* ".repeat(i))
+    i++;
+}
+
+ 
